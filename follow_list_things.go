0dc84c50bd4c73f67fec5facd7a85de116970eb4
AbsoluteZero2A03 <AbsoluteZero2A03@gmail.com>
2016-03-10 07:06:50 -0800

    now can get friends for arbitrary id

--- follow_list_things.go
+++ follow_list_things.go
@@ -1,57 +1,72 @@
 package main
 
 import (
-    "fmt"
-	/*"github.com/dghubble/oauth1"
-	"github.com/dghubble/oauth1/twitter"
-    "golang.org/x/oauth2"*/
-    "bytes"
-    "io/ioutil"
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
-	"net/http/httputil"
-    "compress/gzip"
 	"os"
-    "strings"
-    "encoding/base64"
-	//"reflect"
-    //"strconv"
+	"strings"
 )
-/*
-func getFriends(id string, config oauth1.Config, requestToken string, requestSecret string) {
-    baseUrlStr := "https://api.twitter.com/1.1/friends/ids.json?user_id="
-    token := oauth1.NewToken(requestToken, requestSecret)
-    httpClient := config.Client(oauth1.NoContext,token)
-    resp, _ := httpClient.Get(baseUrlStr + id)
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Printf(string(body))
+
+func getFriends(id string, auth_token string) []uint64 {
+	baseUrlStr := "https://api.twitter.com/1.1/friends/ids.json?user_id="
+	urlStr := baseUrlStr + id
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", urlStr, nil)
+	req.Header.Add("Authorization", "Bearer "+auth_token)
+	req.Header.Add("User-Agent", "DEEP LEARNING v0.0")
+
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	var fl FriendsList
+	json.Unmarshal(body, &fl)
+	var int_ids []uint64
+	for _, elt := range fl.IdList {
+		int_ids = append(int_ids, uint64(elt))
+	}
+	return int_ids
 }
-*/
+
+type TokenContainer struct {
+	AccessToken string `json:"access_token"`
+}
+
+type FriendsList struct {
+	IdList []float64 `json:"ids"`
+}
 
 func main() {
-    consumerKey := strings.TrimSpace(os.Getenv("TWITTER_APP_CONSUMER_KEY"))
-    fmt.Println(consumerKey)
-    consumerSecret := strings.TrimSpace(os.Getenv("TWITTER_APP_CONSUMER_SECRET"))
-    fmt.Println(consumerSecret)
-    fmt.Println(consumerKey + ":" + consumerSecret)
-    consumerString := base64.StdEncoding.EncodeToString([]byte(consumerKey + ":" + consumerSecret))
+	consumerKey := strings.TrimSpace(os.Getenv("TWITTER_APP_CONSUMER_KEY"))
+	fmt.Println(consumerKey)
+	consumerSecret := strings.TrimSpace(os.Getenv("TWITTER_APP_CONSUMER_SECRET"))
+	fmt.Println(consumerSecret)
+	fmt.Println(consumerKey + ":" + consumerSecret)
+	consumerString := base64.StdEncoding.EncodeToString([]byte(consumerKey + ":" + consumerSecret))
 
-    twitterEndpoint := "https://api.twitter.com/oauth2/token"
+	twitterEndpoint := "https://api.twitter.com/oauth2/token"
 
-    client := &http.Client { }
-    req, _ := http.NewRequest("POST", twitterEndpoint, bytes.NewBuffer([]byte("grant_type=client_credentials")))
-    req.Header.Add("Authorization", "Basic "+ consumerString)
-    req.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
-    req.Header.Add("User-Agent", "DEEP LEARNING v0.0")
-    req.Header.Add("Accept-Encoding", "gzip")
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", twitterEndpoint, bytes.NewBuffer([]byte("grant_type=client_credentials")))
+	req.Header.Add("Authorization", "Basic "+consumerString)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+	req.Header.Add("User-Agent", "DEEP LEARNING v0.0")
+	req.Header.Add("Accept-Encoding", "gzip")
 
-    reqText, _ := httputil.DumpRequest(req, true)
-    fmt.Println(string(reqText))
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+	bodyReader, _ := gzip.NewReader(resp.Body)
+	bodyText, _ := ioutil.ReadAll(bodyReader)
 
-    resp, _ := client.Do(req)
-    defer resp.Body.Close()
-    bodyReader, _ := gzip.NewReader(resp.Body)
-    bodyText, _ := ioutil.ReadAll(bodyReader)
+	var m TokenContainer
+	json.Unmarshal(bodyText, &m)
+	fmt.Println(m.AccessToken)
 
-    fmt.Println(string(bodyText))
+	friends := getFriends(os.Args[1], m.AccessToken)
+	fmt.Println(friends)
 }
